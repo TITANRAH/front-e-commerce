d515b7fe073ebc5d56afd6ee51a163edc0539600
Sergio Miranda <152328184+TitanDevelopMan@users.noreply.github.com>
2024-06-15 14:12:04 -0400

    arreglo de next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
     images: {
       remotePatterns: [
         {
-          protocol: "http",
-          hostname: "localhost",
-          port: "1337",
+          protocol: "https",
+          hostname: "res.cloudinary.com",
+          port: "",
           pathname: "**",
         }
        
